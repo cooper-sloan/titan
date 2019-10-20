e1b6e2978ab9867a1e7564009833fb2569e6fe49
cooper-sloan <cooper@isloan.net>
2019-10-19 22:37:13 -0700

    Nut

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public Transform explosionPrefab;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        if (collision.gameObject.GetComponent<Destructable>() != null){
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point;
+            Instantiate(explosionPrefab, pos, rot);
+            Destroy(gameObject);
+        }
+    }
+}
