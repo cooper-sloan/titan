31f26a00b3e06ea8f8ae17ea9852f84d611a26f4
cooper-sloan <cooper@isloan.net>
2019-10-20 20:14:41 -0700

    ballsack

--- Assets/Scripts/Master.cs
+++ Assets/Scripts/Master.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.Networking;
 public class Master : MonoBehaviour
 {
+    public CustomNetworkManager networkManager;
+    public GameObject mobileUI;
     public GameObject vrPlayer;
     public GameObject mobilePlayer;
 
     void Awake(){
         if (XRDevice.isPresent){
             // enable vr player
-            Debug.Log("vr coop is yag");
+            mobileUI.SetActive(false);
+            networkManager.StartHosting();
             vrPlayer.SetActive(true);
         } else{
             // enable mobile player
-            Debug.Log("cooop is mobile personz");
             mobilePlayer.SetActive(true);
         }
     }
