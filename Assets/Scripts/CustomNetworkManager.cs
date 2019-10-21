a33d0b121224ba8067a226c1ec03a7a03c1bd7d3
Diego Lazares <diego.lazares@gmail.com>
2019-10-20 17:30:05 -0700

    Adds MobileNetworkingUI

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+    // Start is called before the first frame update
+    public void StartHosting()
+    {
+        Debug.Log("Start hosting!");
+        base.StartHost();   
+    }
+
+    public void StartClientConnnection()
+    {
+        Debug.Log("Start client!");
+        base.StartClient();
+    }
+
+    public void StartServerOnly()
+    {
+        Debug.Log("Start server!");
+        base.StartServer();
+    }
+
+
+}
