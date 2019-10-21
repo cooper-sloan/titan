7f744098f5e39f05116d735bf927ea6cdf862cf2
dlazares <diego.lazares@gmail.com>
2019-10-20 19:45:25 -0700

    stay initialized

--- Assets/Scripts/CustomNetworkDiscovery.cs
+++ Assets/Scripts/CustomNetworkDiscovery.cs
@@ -4,6 +4,11 @@ using System.Collections;
 
 public class CustomNetworkDiscovery : NetworkDiscovery
 {
+
+    public void Start()
+    {
+        Initialize();
+    }
     public override void OnReceivedBroadcast(string fromAddress, string data)
     {
         Debug.Log("On recieved broadcast with address");
